bd4f869966b4f1dc503ab696b1cc0c672c6a9783
wjsxorjs <wjsxorjs@naver.com>
2024-04-02 16:52:28 +0900

    JAVA 2024.04.02 0.85

--- Ex20240402/src/pm/MyFrameEx4.java
+++ Ex20240402/src/pm/MyFrameEx4.java
@@ -1,11 +1,53 @@
 package pm;
 
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import am.MyWinAdpt;
 
 public class MyFrameEx4 extends JFrame {
-
-	public static void main(String[] args) {
+	
+	
+	JLabel lb1, lb2, lb3, lb4;
+	
+	JPanel p1;
+	
+	public MyFrameEx4() {
 		
+		p1 = new JPanel();
+		
+		ImageIcon icon1 = new ImageIcon("src/images/blog.png");
+		ImageIcon icon2 = new ImageIcon("src/images/cafe.png");
+		ImageIcon icon3 = new ImageIcon("src/images/mail.png");
+		ImageIcon icon4 = new ImageIcon("src/images/stock.png");
+		
+		//생성된 image 아이콘을 lb1 생성할 때 넣어준다.
+		lb1 = new JLabel(icon1); // Icon은 ImagesIcon의 조상
+		lb2 = new JLabel(icon2);
+		lb3 = new JLabel(icon3);
+		lb4 = new JLabel(icon4);
+		
+		p1.add(lb1);
+		p1.add(lb2);
+		p1.add(lb3);
+		p1.add(lb4);
+		
+		this.add(p1,BorderLayout.CENTER);
+		
+		this.setBounds(300, 200, 350, 250);
+		this.setVisible(true);
+		
+		this.addWindowListener(new MyWinAdpt());
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		new MyFrameEx4();
 
 	}
 
---
+++ Ex20240402/src/pm/MyFrameEx5.java
@@ -0,0 +1,12 @@
+package pm;
+
+import javax.swing.JFrame;
+
+public class MyFrameEx5 extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
