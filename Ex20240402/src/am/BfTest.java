cb0c886e9318912465f0e2c5bff125b0e5d061f0
wjsxorjs <wjsxorjs@naver.com>
2024-04-02 09:57:11 +0900

    JAVA 2024.04.02 0.1

--- Ex20240402/src/am/BfTest.java
+++ Ex20240402/src/am/BfTest.java
@@ -4,7 +4,10 @@ import java.awt.Color;
 
 import javax.swing.JFrame;
 
-public class BfTest extends JFrame {
+public class BfTest extends JFrame { 	// 재정의는 상속관계에 있는 서로 다른 클래스끼리
+										// 부모클래스의 메소드를 자식클래스에서 다시 정의하는 것이며
+										// 부모클래스의 메소드는 숨김 처리(Hidden)가 되고
+										// 우선순위가 자식클래스에서 재정의한 메소드로 주어진다.
 	
 	BfTestWL btWL;
 	
@@ -16,7 +19,9 @@ public class BfTest extends JFrame {
 				
 		btWL = new BfTestWL();
 		
-		this.addWindowListener(btWL);
+		this.addWindowListener(btWL);	// WindowListener를 구현한 객체(의 주소) < 중요하다.
+									 	//											ㄴWindowListener는 인터페이스이므로 인자로 사용이 어려움
+										//												ㄴ다만, 사용범위는 인자의 자료형인 WindowListener로 제한
 		
 		this.setVisible(true);
 	}
--- Ex20240402/src/am/BfTestWL.java
+++ Ex20240402/src/am/BfTestWL.java
@@ -3,7 +3,11 @@ package am;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class BfTestWL implements WindowListener {
+public class BfTestWL implements WindowListener {//	: 인터페이스 - 구현(implements)
+//						extends 					: 추상클래스 - 상속(extends)
+//						추상화 두 가지 모두 추상메소드는 모두 재정의해야한다.
+	
+	
 
 	@Override
 	public void windowOpened(WindowEvent e) {
